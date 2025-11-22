b8b94fb20faa10b5d95e02d8c3728c2be12aba55
Aditya Patra <adityabpatra@gmail.com>
2025-11-22 15:24:47 -0800

    choosing to simulate computer network grid

--- main.cpp
+++ main.cpp
@@ -88,7 +88,7 @@ public:
 
     // Print the graph's adjacency list
     void printGraph() {
-        cout << "Graph's adjacency list: " << endl;
+        cout << "Computer Network Grid: " << endl;
         for (int i = 0; i < adjList.size(); i++) {
             cout << i << " --> ";
             for (Pair v : adjList[i])
