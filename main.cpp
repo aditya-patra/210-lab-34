38f31a4210fd1f04f242527ec2f03c6e83df11b0
Aditya Patra <adityabpatra@gmail.com>
2025-11-22 16:45:11 -0800

    (Previous menu copied from sample, will change it to match my simulation

--- main.cpp
+++ main.cpp
@@ -249,7 +249,7 @@ int main() {
     // menu
     int choice = -1;
     while (choice != 6) {
-        cout << "[1] Display water distribution network\n[2] Check contaminant spread (BFS)\n[3] Plan inspection route  (DFS)\n[4] Calculate shortest paths\n[5] Find Minimum Spanning Tree\n[0] Exit" << endl;
+        cout << "[1] Display water distribution network\n[2] Check contaminant spread (BFS)\n[3] Plan inspection route  (DFS)\n[4] Calculate shortest paths\n[5] Find Minimum Spanning Tree\n[6] Exit " << endl;
     }
 
     return 0;
